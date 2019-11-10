1d4b382649c90f518d89f7127a9b12e352ff9686
Romario Gustave <romagustave10@gmail.com>
2019-11-10 00:41:47 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
+def inte 
+  #write your code here
+  puts " Enter your seconds"
+  num = gets.chomp 
+  end
 
 def countdown
+  counter = 0
+  while counter < num
+  puts "#{num} seconds"
+  counter -=1
+  if counter= 0 
+    puts "Happy new year"
+end
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+  puts "#{seconds} seconds"
+  second -= 0
+end 
+return "Happy new  year"
 end
